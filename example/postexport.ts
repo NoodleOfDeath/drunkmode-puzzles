30038904d190a383da092c3ff8b1c74d16d6fc3e
noodleofdeath <thom@noodleofdeath.com>
2024-03-26 13:19:27 -0400

    example updates

--- example/postexport.ts
+++ example/postexport.ts
@@ -1,4 +1,4 @@
 import fs from 'fs';
 
 const text = fs.readFileSync('out/index.html', 'utf8');
-fs.writeFileSync('out/index.html', text.replace(/src="\//g, 'src="').replace(/ crossorigin=""/g, ''), 'utf8');
+fs.writeFileSync('out/index.html', text.replace(/src="\//g, 'src="./').replace(/ crossorigin=""/g, ''), 'utf8');
--- example/src/pages/index.tsx
+++ example/src/pages/index.tsx
@@ -26,35 +26,80 @@ const StyledButton = styled.button`
   font-size: 1.5rem
 `;
 
+type Size = {
+  width: number;
+  height: number;
+};
+
+const ZERO_SIZE: Size = { height: 0, width: 0 };
+
+type Point = {
+  x: number;
+  y: number;
+};
+
 export default function Home() {
   
-  const [size, setSize] = React.useState([0, 0]);
-  const [coords, setCoords] = React.useState<{ x: number, y: number }>();
+  const [size, setSize] = React.useState(ZERO_SIZE);
+  const [coords, setCoords] = React.useState<Point>();
+  const [yesCount, setYesCount] = React.useState(0);
+  const yesButtonRef = React.useRef<HTMLButtonElement>(null);
+  
+  const moveButton = React.useCallback((size: Size) => {
+    const buttonDims = yesButtonRef.current?.getBoundingClientRect() ?? ZERO_SIZE;
+    const padding = 10;
+    setCoords({
+      x: Math.floor(Math.random() * (size.width - buttonDims.width)) + padding,
+      y: Math.floor(Math.random() * (size.height - buttonDims.height)) + padding,
+    });
+  }, []);
   
   React.useLayoutEffect(() => {
     function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
+      setSize((prev) => {
+        if (prev.width === window.innerWidth && 
+            prev.height === window.innerHeight) {
+          return prev;
+        }
+        const newSize = {
+          height: window.innerHeight,
+          width: window.innerWidth,
+        };
+        if (coords) {
+          moveButton(newSize);
+        }
+        return newSize;
+      });
     }
     window.addEventListener('resize', updateSize);
     updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
+    return () => {
+      window.removeEventListener('resize', updateSize);
+    };
+  }, [coords, moveButton]);
+
+  React.useEffect(() => {
+    if (yesCount > 0 && yesCount % 5 === 0) {
+      PuzzleMessage.onFailure({ message: 'Bruh, stop cappin\'! You know you ain\'t sober!' });
+    }
+  }, [yesCount]);
   
   return (
-    <StyledMain height={ size[1] }>
+    <StyledMain height={ size.height }>
       <StyledContainer>
-        <StyledTitle>Are you sober?</StyledTitle>
+        <StyledTitle>
+          Are you sober?
+        </StyledTitle>
         <StyledButton
+          ref={ yesButtonRef }
           style={ {
             left: coords?.x,
             position: coords ? 'absolute' : undefined,
             top: coords?.y,
           } }
           onClick={ () => {
-            setCoords({
-              x: Math.floor(Math.random() * (size[0] - 100)) + 10,
-              y: Math.floor(Math.random() * (size[1] - 50)) + 10,
-            });
+            setYesCount((prev) => prev + 1);
+            moveButton(size);
           } }>
           Yes
         </StyledButton>
