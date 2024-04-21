ca16f82247b3e2e75900559fa9665d8b03199978
tris <tris.bouden@gmail.com>
2024-04-21 20:50:17 +0100

    add react-beautiful-dnd for dragging Cards

--- examples/solitaire/src/components/Tableau.tsx
+++ examples/solitaire/src/components/Tableau.tsx
@@ -1,4 +1,12 @@
+//Tableau.tsx
 import React from 'react';
+
+import dynamic from 'next/dynamic';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+} from 'react-beautiful-dnd';
 
 import Card from './Card';
 
@@ -9,7 +17,7 @@ interface TableauProps {
 }
 
 const Tableau: React.FC<TableauProps> = ({ cards }) => {
-  const tableauCards = [];
+  const tableauCards: Array<Array<CardType>> = [];
   let currentIndex = 0;
   for (let i = 1; i <= 7; i++) {
     const column = [];
@@ -20,23 +28,34 @@ const Tableau: React.FC<TableauProps> = ({ cards }) => {
     currentIndex += i;
   }
   return (
-    
-    <div className="grid w-full grid-cols-7 justify-items-center">
-      {tableauCards.map((column, columnIndex) => (
-        <div style={ { height: `${(column.length - 1) * 20 + 150}px` } } key={ columnIndex }>
-          {column.map((card, cardIndex) => {
-            const isLastCard = cardIndex === column.length - 1;
-            return (
-              <div
-                key={ cardIndex }
-                style={ { transform: `translateY(${cardIndex * -75}%)` } }>
-                <Card { ...card } isFaceUp={ isLastCard } />
+    <DragDropContext onDragEnd={ () => console.log('ends') }>
+      <div className="grid w-full grid-cols-7 justify-items-center">
+        {tableauCards.map((column, columnIndex) => (
+          <Droppable droppableId={ `col-${columnIndex}` } key={ columnIndex }>
+            {(provided) => (
+              <div { ...provided.droppableProps } ref={ provided.innerRef } style={ { height: `${(column.length - 1) * 20 + 150}px` } }>
+                {column.map((card, cardIndex) => {
+                  const isLastCard = cardIndex === column.length - 1;
+                  return (
+                    <Draggable draggableId={ `${cardIndex}` } index={ cardIndex } key={ cardIndex }>
+                      {(provided) => (
+                        <div
+                          ref={ provided.innerRef }
+                          { ...provided.draggableProps }
+                          style={ { transform: `translateY(${cardIndex * -75}%)` } }>
+                          <Card { ...card } isFaceUp={ isLastCard } />
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                })} 
               </div>
-            );
-          })} 
-        </div>
-      ))}
-    </div>
+            )}
+            
+          </Droppable>
+        ))}
+      </div>
+    </DragDropContext>
   );
 };
 
