0f8ed2bc7e9ab5ae9be4cb29393d8d5f02bc6fdc
noodleofdeath <thom@noodleofdeath.com>
2025-12-23 06:28:47 -0500

    update dev tools

--- dev-tools/export.ts
+++ dev-tools/export.ts
@@ -1,13 +1,14 @@
+import { execSync } from 'child_process';
 import fs from 'fs';
 import p from 'path';
-import { execSync } from 'child_process';
 
 const EXAMPLES_DIR = 'examples';
+const OUT_DIR = p.resolve(p.join(__dirname, '../out'));
 
 const examples = fs.readdirSync(EXAMPLES_DIR);
 
 for (const example of examples) {
-  const cmd = `(cd ${p.join(EXAMPLES_DIR, example)} && yarn export)`;
+  const cmd = `(cd ${p.join(EXAMPLES_DIR, example)} && yarn export && cd out && zip -r ${p.join(OUT_DIR, `${example}.zip`)} .)`;
   console.log(cmd);
   execSync(cmd);
 }
